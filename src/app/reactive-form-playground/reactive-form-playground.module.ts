8510dec90fc32a12715b185766bd78baff6292cd
Kimserey <kimserey.lam@afusion.com>
2017-06-07 11:30:27 +0800

    more form

--- src/app/reactive-form-playground/reactive-form-playground.module.ts
+++ src/app/reactive-form-playground/reactive-form-playground.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SimpleFormComponent } from './simple-form.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     ReactiveFormsModule
   ],
   declarations: [
+    SimpleFormComponent
   ],
+  exports: [
+    SimpleFormComponent
+  ]
 })
 export class ReactiveFormPlaygroundModule { }
--- src/app/reactive-form-playground/simple-form.component.ts
+++ src/app/reactive-form-playground/simple-form.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-simple-form',
   templateUrl: 'simple-form.component.html',
 })
 export class SimpleFormComponent {
+  recipeForm: FormGroup;
+
+  get ingredients(): FormArray {
+    return this.recipeForm.get('ingredients') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {
+    const ingredients = [ 'carrot', 'beans' ];
+    const ingredientFromGroups = ingredients.map(i => fb.group({ ingredientName: i }));
+
+    this.recipeForm = fb.group({
+      profile: fb.group({
+        name: ['', Validators.required ],
+        timeEstimation: ''
+      }),
+      ingredients: fb.array(ingredientFromGroups)
+    });
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.fb.group({
+      ingredientName: ''
+    }));
+  }
+
+  removeIngredientAtIndex(index) {
+    this.ingredients.removeAt(index);
+  }
 }
